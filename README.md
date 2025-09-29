7a387015596ebc15ef9a07195e4e4d3336ca2740
purr-purr <56505654+purr-purr@users.noreply.github.com>
2025-09-30 00:17:12 +0100

    feat: add new info

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ You can run and build application with the following commands
 ```
 yarn install
 
+nvm use 16.6.0
+
 yarn start
 
 yarn build
--- src/helpers/const.ts
+++ src/helpers/const.ts
@@ -1,8 +1,8 @@
 export const MOBILE_BP = 1124;
-export const ADDRESS = '01014, м. Київ, вул. Звіринецька, буд. 63 офіс 1';
+export const ADDRESS = '03056, місто Київ, вул.Борщагівська, будинок 154';
 export const ADDRESS_LINK = 'https://goo.gl/maps/cS5ouxerbHJcq7XL8';
 export const TAX_CODE = 44765447;
-export const OWNER = 'Фетісова Людмила Василівна';
-export const CURRENT_ACCOUNT = 'UA 54 320478 0000026004924930157';
+export const OWNER = 'Чорний Микола Миколайович';
+export const CURRENT_ACCOUNT = 'UA 63 322669 0000026008300645911 АТ «ОЩАДБАНК»';
 export const CONTACT_EMAIL = 'bf_uwwt@ukr.net';
 export const CONTACT_NUMBER = 380931776909;
