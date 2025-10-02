0861cd1e3c86d98e16ce9d3c56985d1f16e72569
purr-purr <56505654+purr-purr@users.noreply.github.com>
2025-10-02 17:14:05 +0100

    fix: contact info

--- src/helpers/const.ts
+++ src/helpers/const.ts
@@ -1,6 +1,6 @@
 export const MOBILE_BP = 1124;
-export const ADDRESS = '03056, місто Київ, вул.Борщагівська, будинок 154';
-export const ADDRESS_LINK = 'https://goo.gl/maps/cS5ouxerbHJcq7XL8';
+export const ADDRESS = '01135, м. Київ, Шевченківський р-н., вул. Полтавська, б.10, оф. 302';
+export const ADDRESS_LINK = 'https://maps.app.goo.gl/87tDrTW19Say8GzY7';
 export const TAX_CODE = 44765447;
 export const OWNER = 'Чорний Микола Миколайович';
 export const CURRENT_ACCOUNT = 'UA 63 322669 0000026008300645911 АТ «ОЩАДБАНК»';
